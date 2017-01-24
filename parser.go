92fabbae571d28d380141bd4a95b8c890ae85d33
Miana le Fay <v0lva@yandex.ru>
2017-01-24 02:01:13 +0100

    Parse applications
    Pretty simple. Remember last expression in `expr`. On every variable either a) use this variable as latest, unless b) there is already one stored, then apply this variable to previous expression. Green!

--- parser.go
+++ parser.go
@@ -7,15 +7,29 @@ import (
 type Parser struct{}
 
 func (prsr *Parser) Parse(input io.RuneScanner) (Λ, error) {
-	var scnr Scanner
+	var (
+		scnr Scanner
+		expr Λ
+	)
 
 	for {
 		token := scnr.Scan(input)
 
 		switch token {
-		case EOF, LAMBDA, DOT, LPAREN, RPAREN:
+		case LAMBDA, DOT, LPAREN, RPAREN:
+		case EOF:
+			return expr, nil
 		default:
-			return &Variable{string(token)}, nil
+			thisVar := &Variable{string(token)}
+
+			if expr == nil {
+				expr = thisVar
+			} else {
+				expr = &Application{expr, thisVar}
+			}
 		}
 	}
+}
+
+type layer struct {
 }
