1c31644b348a3ec26a3a68ac66662da318e8d922
Miana le Fay <v0lva@yandex.ru>
2017-01-24 20:03:47 +0100

    Hook parser with evaluation
    Stub of `EvalString` is enough to fail the right test. Stupid as it may sound, we are on the right track. Next step, supervising parser flow!

--- vm.go
+++ vm.go
@@ -1,16 +1,47 @@
 package λ
 
-import "errors"
+import (
+	"bufio"
+	"bytes"
+	"errors"
+)
 
 type VM struct {
+	expr   Λ
+	parser Parser
 }
 
 func (vm *VM) EvalString(src string) (error, Trace) {
-	return errors.New("not implemented yet"), nil
+	sv := make(chan Report)
+	vm.parser.Supervisor = sv
+
+	go func() {
+		for _ = range sv {
+		}
+	}()
+
+	input := bufio.NewReader(bytes.NewReader([]byte(src)))
+	expr, pos, err := vm.parser.Parse(input)
+
+	if err != nil {
+		return err, &trace{pos: pos}
+	}
+
+	vm.expr = expr
+
+	return nil, nil
 }
 
 var UnexpectedFreeVariable = errors.New("unexpected free variable")
 
 type Trace interface {
 	Pos() int
+}
+
+type trace struct {
+	pos int
+}
+
+func (trc *trace) Pos() int {
+	return trc.pos
 }
