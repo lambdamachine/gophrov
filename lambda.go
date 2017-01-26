30e7371d2f18955712cf3327f1e22a5b4243e1c1
Miana le Fay <v0lva@yandex.ru>
2017-01-26 21:11:55 +0100

    Combinator
    Lets start quick and dirty, with simple combinator structure. Lets don't bother with performance for now, simple mutable linked list as memory will do for now.

--- lambda.go
+++ lambda.go
@@ -1,11 +1,13 @@
 package λ
 
+import "errors"
+
 type Λ interface {
 	Call(Λ) Λ
 }
 
 func NewΛ(label string, fn func(μ Μ) Λ) Λ {
-	return I
+	return &combinator{nil, &meta{label, fn}, nil}
 }
 
 type Μ interface {
@@ -20,3 +22,34 @@ func (_ *identity) Call(λ Λ) Λ {
 }
 
 var I *identity = nil
+
+type combinator struct {
+	parent *combinator
+	meta   *meta
+	λ      Λ
+}
+
+type meta struct {
+	label string
+	fn    func(μ Μ) Λ
+}
+
+func (cbntr *combinator) NewΛ(label string, fn func(μ Μ) Λ) Λ {
+	return &combinator{cbntr, &meta{label, fn}, nil}
+}
+
+func (cbntr *combinator) Call(λ Λ) Λ {
+	return cbntr.meta.fn(&combinator{cbntr, cbntr.meta, λ})
+}
+
+func (cbntr *combinator) Read(label string) Λ {
+	for current := cbntr; current != nil; current = current.parent {
+		if current.meta.label == label {
+			return current.λ
+		}
+	}
+
+	panic(InconsistentMemory)
+}
+
+var InconsistentMemory = errors.New("inconsistent memory")
